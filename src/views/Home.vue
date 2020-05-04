9553533fd1733467df14b994376884f55ad2806c
marcozefanya <marcoabel25@gmail.com>
2020-05-04 13:42:57 +0700

    Mulai membuat sistem polling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+  	<section class="vote">
+  		<h3 class="give-your-vote">
+  			Silahkan berikan penialaian anda terhadap pelayanan kami.
+  		</h3>
+
+  		<div class="emoticons">
+  			ini akan menjadi komponen vote
+  		</div>
+  	</section>
+
+  	<section class="finish">
+  		
+  		<h4>Terima kasih telah memberikan penilaian anda</h4>
+
+  	</section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+<style type="text/css">
+	.container{
+		display: flex;
+		height: 100vh;
+		flex-wrap: wrap;
+		align-items: center;
+	}
+	section{
+		width: 100%;
+	}
+</style>
